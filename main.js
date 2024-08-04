dbd66f22f9629abce954ed5b235e752353f76809
Isabella-mg <isabellamartinez@uadec.edu.mx>
2024-08-04 16:23:34 -0600

    agregamos Typewriter

---
+++ main.js
@@ -0,0 +1,16 @@
+// console.log("Hola Mundo! desde la consola");
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Desarrolladora Frontend Jr. | Arquitecta')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('¡Si lo puedes imaginar, lo puedes crear!')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong> programar!</strong>')
+    .pauseFor(2500)
+    .start();
